70f3a95f550c5e3f8c1ceee74a17ebb4c697c358
Megidd Git <megiddgit@gmail.com>
2022-02-06 04:59:22 +0330

    Demo works. Modified according to:
    https://collab-project.github.io/videojs-wavesurfer/#/react
    
    https://stackoverflow.com/q/65255315/3405291

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'video.js/dist/video-js.css';
+import videojs from 'video.js';
+
+import WaveSurfer from 'wavesurfer.js';
+
+/*
+// the following imports are only needed when you're using
+// the microphone plugin
+import 'webrtc-adapter';
+
+import MicrophonePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.microphone.js';
+WaveSurfer.microphone = MicrophonePlugin;
+*/
+
+// register videojs-wavesurfer plugin with this import
+import 'videojs-wavesurfer/dist/css/videojs.wavesurfer.css';
+import Wavesurfer from 'videojs-wavesurfer/dist/videojs.wavesurfer.js';
+
+class App extends Component {
+    componentDidMount() {
+        // instantiate Video.js
+        this.player = videojs(this.audioNode, this.props, () => {
+            // print version information at startup
+            const version_info = 'Using video.js ' + videojs.VERSION +
+                ' with videojs-wavesurfer ' + videojs.getPluginVersion('wavesurfer') +
+                ', wavesurfer.js ' + WaveSurfer.VERSION + ' and React ' + React.version;
+            videojs.log(version_info);
+
+            // load file
+            this.player.src({src: 'hal.wav', type: 'audio/wav'});
+        });
+
+        this.player.on('waveReady', (event) => {
+            console.log('waveform: ready!');
+        });
+
+        this.player.on('playbackFinish', (event) => {
+            console.log('playback finished.');
+        });
+
+        // error handling
+        this.player.on('error', (element, error) => {
+            console.error(error);
+        });
+    }
+
+    // destroy player on unmount
+    componentWillUnmount() {
+        if (this.player) {
+            this.player.dispose();
+        }
+    }
+    render() {
+        return (
+        <div data-vjs-player>
+            <audio id="myAudio" ref={node => this.audioNode = node} className="video-js vjs-default-skin"></audio>
+        </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+
+const videoJsOptions = {
+    controls: true,
+    bigPlayButton: false,
+    width: 600,
+    height: 300,
+    fluid: false,
+    plugins: {
+        wavesurfer: {
+            backend: 'MediaElement',
+            displayMilliseconds: true,
+            debug: true,
+            waveColor: '#163b5b',
+            progressColor: 'black',
+            cursorColor: 'black',
+            hideScrollbar: true
+        }
+    }
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App { ...videoJsOptions }/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
